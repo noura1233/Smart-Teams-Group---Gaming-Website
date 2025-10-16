6baab55712a04af73b4aee4e41b63c5bb7b5fa53
Paul Daly - STUDENT <g00470372@atu.ie>
2025-10-16 10:32:40 +0100

    Very basic layout working

--- gamesWebsiteProject/21CardSim/21Simulator.py
+++ gamesWebsiteProject/21CardSim/21Simulator.py
@@ -1,3 +1,24 @@
-#import sys
+import os
+clear = lambda: os.system('cls') #Creates obj for clearing serial screen
 
-print("Test")
+#Variables
+state = 0 #Controls what part of game
+#0 - Title, 1 - Setup, 2 - Start, MORE TO BE ADDED, 9 - End
+win = 21
+
+
+while True:
+    if(state==0):
+        clear()
+        print("TItle Screen")
+        choice = input("Play? (y/n) > ")
+        if(choice=="y"):
+            state = 1
+        elif(choice=="n"):
+            state = 9
+
+
+
+    if(state==9):
+        print("Program end")
+        break
